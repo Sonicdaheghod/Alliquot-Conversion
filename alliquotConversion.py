ffe5664a1d9ade9f1c817548d7d06d8f57cf1173
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-04-19 20:25:49 -0400

    Update alliquotConversion.py

--- alliquotConversion.py
+++ alliquotConversion.py
@@ -1,22 +1,36 @@
 #Program to know how much solvent to put in fractions for tests (bioassay, LCMS, etc.)
 #This program is specific for my bioassay preperation for my fungal samples I am working on for my honors thesis research project on reisolating metabolites
 
-import pandas as pd
+mport pandas as pd
 while True:
     print("""-------------- Alliquot Calculator --------------
     """)
 
-    fractionNumber = int(input("How many fractions do you have? "))
+    #input only accepts number inputs
+    try:
+        fractionNumber = int(input("How many fractions do you have? "))
+        
+    except ValueError:
+        print("Invalid response. Type in a number.")
+        continue
+        
     sampleMass = []
     solventNeeded = []
 
+  
     for i in range(fractionNumber):
+        
         #print mass in mg
         sampleMassInput = input("Enter the mass of your sample (in g): ")
+
+       
+        
         calculationMass = float(sampleMassInput)*1000
         sampleMass.append(calculationMass)
         #use concentration to print out desired amoutn solvent
         sampleConcentrationInput = input("What concentration do you need to dilute your sample in? (mg/ml) ")
+
+
         calculationConcentrationInput = float((float(sampleMassInput)/float(sampleConcentrationInput))*1000)
         solventNeeded.append(calculationConcentrationInput)
 
